40329c6e14e080c2a00825564d778077138bf3eb
Aarti Ghatkesar <aarti@cerebras.net>
2025-07-24 15:13:30 -0700

    Fix sft_lossmask - remove hardcode

--- src/llamafactory/data/loader.py
+++ src/llamafactory/data/loader.py
@@ -281,7 +281,7 @@ def get_dataset(
     model_args: "ModelArguments",
     data_args: "DataArguments",
     training_args: "Seq2SeqTrainingArguments",
-    stage: Literal["pt", "sft", "rm", "ppo", "kto"],
+    stage: Literal["pt", "sft", "rm", "ppo", "kto", "sft_lossmask"],
     tokenizer: "PreTrainedTokenizer",
     processor: Optional["ProcessorMixin"] = None,
 ) -> "DatasetModule":
--- src/llamafactory/train/sft/workflow.py
+++ src/llamafactory/train/sft/workflow.py
@@ -48,7 +48,7 @@ def run_sft(
     tokenizer_module = load_tokenizer(model_args)
     tokenizer = tokenizer_module["tokenizer"]
     template = get_template_and_fix_tokenizer(tokenizer, data_args)
-    dataset_module = get_dataset(template, model_args, data_args, training_args, stage="sft", **tokenizer_module)
+    dataset_module = get_dataset(template, model_args, data_args, training_args, stage=finetuning_args.stage, **tokenizer_module)
     model = load_model(tokenizer, model_args, finetuning_args, training_args.do_train)
 
     if getattr(model, "is_quantized", False) and not training_args.do_train:
