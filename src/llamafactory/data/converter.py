b27fbb6e3f669122fe30e763ae51a93f592e04a4
Aarti Ghatkesar <aarti@cerebras.net>
2025-07-24 15:14:06 -0700

    Handle - find_media for base64 images

--- src/llamafactory/data/converter.py
+++ src/llamafactory/data/converter.py
@@ -54,13 +54,16 @@ class DatasetConverter:
         if self.dataset_attr.load_from in ["script", "file"]:
             if isinstance(medias[0], str):
                 for i in range(len(medias)):
-                    media_path = os.path.join(self.data_args.media_dir, medias[i])
-                    if os.path.isfile(media_path):
-                        medias[i] = media_path
+                    if ";base64" in medias[i]: # base64 encoded media
+                        continue
                     else:
-                        logger.warning_rank0_once(
-                            f"Media {medias[i]} does not exist in `media_dir`. Use original path."
-                        )
+                        media_path = os.path.join(self.data_args.media_dir, medias[i])
+                        if os.path.isfile(media_path):
+                            medias[i] = media_path
+                        else:
+                            logger.warning_rank0_once(
+                                f"Media {medias[i]} does not exist in `media_dir`. Use original path."
+                            )
             elif isinstance(medias[0], list):  # for processed video frames
                 # medias is a list of lists, e.g., [[frame1.jpg, frame2.jpg], [frame3.jpg, frame4.jpg]]
                 for i in range(len(medias)):
